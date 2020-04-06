9f4da0e3ce77c3ff01f722621724e8f8ea51506d
zhoukunyang <zhoukunyang@aistrong.com>
2020-04-06 16:04:31 +0800

    java代码

---
+++ test/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @author zhoukunyang zhoukunyang@aistrong.com
+ * @date 2020/4/6 16:03
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
