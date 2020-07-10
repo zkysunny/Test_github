ee394bb6f11b4dcb06c4d7405d3119da7fc9a82e
zhoukunyang <zhoukunyang@aistrong.com>
2020-07-11 01:11:51 +0800

    测试git建立分支

---
+++ test/People.java
@@ -0,0 +1,24 @@
+/**
+ * @author zhoukunyang zhoukunyang@aistrong.com
+ * @date 2020/7/11 1:10
+ */
+public class People {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
